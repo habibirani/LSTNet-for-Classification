f089449474753923360a3290b60ddc60cac884ab
Habib Irani <44724668+habibirani@users.noreply.github.com>
2023-11-12 23:17:49 -0600

    Update LSTNet.py

--- LSTNet.py
+++ LSTNet.py
@@ -3,46 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LSTNetForClassification(nn.Module):
-    def __init__(self, args):
-        super(LSTNetForClassification, self).__init()
+    def __init__(self, args, data):
+        super(ClassificationModel, self).__init__()
         self.use_cuda = args.cuda
         self.P = args.window
+        self.m = data.m  # Update with the number of classes
         self.hidR = args.hidRNN
         self.hidC = args.hidCNN
         self.hidS = args.hidSkip
         self.Ck = args.CNN_kernel
         self.skip = args.skip
-        self.pt = (self.P - self.Ck) // self.skip
+        self.pt = (self.P - self.Ck) / self.skip
         self.hw = args.highway_window
-        self.conv1 = nn.Conv2d(1, self.hidC, kernel_size=(self.Ck, self.m))
+        self.conv1 = nn.Conv2d(1, self.hidC, kernel_size=(self.Ck, 1))
         self.GRU1 = nn.GRU(self.hidC, self.hidR)
         self.dropout = nn.Dropout(p=args.dropout)
-
-        if (self.skip > 0):
+        if self.skip > 0:
             self.GRUskip = nn.GRU(self.hidC, self.hidS)
-            self.linear1 = nn.Linear(self.hidR + self.skip * self.hidS, args.num_classes)  # Output num_classes for classification
+            self.linear1 = nn.Linear(self.hidR + self.skip * self.hidS, self.m)
         else:
-            self.linear1 = nn.Linear(self.hidR, args.num_classes)  # Output num_classes for classification
-
-        if (self.hw > 0):
-            self.highway = nn.Linear(self.hw, args.num_classes)  # Output num_classes for classification
+            self.linear1 = nn.Linear(self.hidR, self.m)
+        if self.hw > 0:
+            self.highway = nn.Linear(self.hw, 1)
+        self.output_layer = nn.Linear(self.hidR, self.m)
+        self.softmax = nn.Softmax(dim=1)  # Softmax for classification
 
     def forward(self, x):
         batch_size = x.size(0)
-
-        # CNN
-        c = x.view(-1, 1, self.P, self.m)
+        c = x.view(-1, 1, self.P, 1)
         c = F.relu(self.conv1(c))
         c = self.dropout(c)
         c = torch.squeeze(c, 3)
-
-        # RNN
         r = c.permute(2, 0, 1).contiguous()
         _, r = self.GRU1(r)
         r = self.dropout(torch.squeeze(r, 0))
-
-        # Skip-RNN
-        if (self.skip > 0):
+        if self.skip > 0:
             s = c[:, :, int(-self.pt * self.skip):].contiguous()
             s = s.view(batch_size, self.hidC, self.pt, self.skip)
             s = s.permute(2, 0, 3, 1).contiguous()
@@ -51,14 +46,14 @@ class LSTNetForClassification(nn.Module):
             s = s.view(batch_size, self.skip * self.hidS)
             s = self.dropout(s)
             r = torch.cat((r, s), 1)
-
         res = self.linear1(r)
-
-        # Highway
-        if (self.hw > 0):
+        if self.hw > 0:
             z = x[:, -self.hw:, :]
             z = z.permute(0, 2, 1).contiguous().view(-1, self.hw)
             z = self.highway(z)
-
-        return F.log_softmax(res, dim=1)  # Use log-softmax for multi-class classification
+            z = z.view(-1, self.m)
+            res = res + z
+        output = self.output_layer(res)
+        output = self.softmax(output)
+        return output
 
